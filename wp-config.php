6e11922107b8f6093ac625779b7c6f5ae79a4061
David Renshaw <dwrenshaw@gmail.com>
2014-07-17 16:58:15 +0000

    delete unused mysql config

--- wp-config.php
+++ wp-config.php
@@ -40,25 +40,6 @@ if ('https' == substr($sandstorm_base_path, 0, 5)) {
   $_SERVER['HTTPS'] = 'on';
 }
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
-
-/** MySQL database username */
-define('DB_USER', 'username_here');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
