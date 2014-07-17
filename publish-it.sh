2b17690a2b4f6800b62d8ba111fa37dcd1bfde00
David Renshaw <dwrenshaw@gmail.com>
2014-07-17 14:33:00 +0000

    clean up wgetted file names

--- publish-it.sh
+++ publish-it.sh
@@ -4,7 +4,17 @@ TMPDIR=/var/www_`date +%s%N`
 URL=http://127.0.0.1:10000/
 
 rm -rf $TMPDIR
-wget --mirror -p --html-extension --convert-links -np -nH -e robots=off -P $TMPDIR $URL
+wget --mirror -p --convert-links -np -nH -e robots=off -P $TMPDIR $URL
+for q in `find $TMPDIR -name '*\?*'`; do
+  if [ -f ${q%\?*} ]; then
+     echo 'removing $q';
+     rm $q;
+  else
+    echo 'renaming $q';
+    mv $q ${q%\?*};
+  fi;
+done
+
 
 mv $STATICDIR ${STATICDIR}_tmp
 mv $TMPDIR $STATICDIR
