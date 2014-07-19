974ab1024b977566fc45c55301c4abcdfbdd7516
David Renshaw <dwrenshaw@gmail.com>
2014-07-19 17:45:42 +0000

    allow larger uploads. exlude access to wp-admin

--- publish-it.sh
+++ publish-it.sh
@@ -4,7 +4,7 @@ TMPDIR=/var/www_`date +%s%N`
 URL=http://127.0.0.1:10000/
 
 rm -rf $TMPDIR
-wget --mirror -p --convert-links -np -nH -e robots=off -P $TMPDIR $URL
+wget --mirror -p --convert-links -np -nH -e robots=off -X /wp-admin/ -P $TMPDIR $URL
 for q in `find $TMPDIR -name '*\?*'`; do
   if [ -f ${q%\?*} ]; then
      echo 'removing $q';
