eca399914d66f9462f0cf463de8a9f245fd7d066
David Renshaw <dwrenshaw@gmail.com>
2014-07-20 22:48:48 +0000

    symlink to readonly version of sandstorm plugin

--- start.sh
+++ start.sh
@@ -8,5 +8,9 @@ mkdir -p /var/wordpress/wp-content
 
 cp -r /wordpress/wp-content-read-only/* /var/wordpress/wp-content
 
+# Link back to the read-only version of the sandstorm plugin, to allow easy updating.
+rm -r /var/wordpress/wp-content/plugins/sandstorm
+ln -s /wordpress/wp-content-read-only/plugins/sandstorm /var/wordpress/wp-content/plugins/sandstorm
+
 ./continue.sh
 
